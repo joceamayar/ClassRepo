664846d5d781123e09649e2f99b4eb6ce8e6aa35
joceamayar <jocelin@tavlee.com>
2023-08-04 09:58:31 -0400

    review

--- 03-JavaScript/01-Activities/28-Stu_Mini-Project/script.js
+++ 03-JavaScript/01-Activities/28-Stu_Mini-Project/script.js
@@ -1,47 +1,73 @@
 
-let choices = ["R", "P", "S"];
+// let choices = ["R", "P", "S"];
+// let wins = 0;
+// let losses = 0;
+// let ties = 0;
+
+// let roundNumber = 0;
+
+// let playerChoice = prompt("Enter 'R' for Rock, 'P' for Paper, or 'S' for Scissors")
+
+// let randomIndex = Math.floor(Math.random() * choices.length)
+// let computerChoice = choices[randomIndex];
+
+// let result = "";
+
+// play()
+
+// function play() {
+
+//   if (playerChoice === "R" && computerChoice === "P") {
+
+//     console.log("player wins!")
+
+//     wins++
+//   }
+//   else if (
+//     (playerChoice === "R" && computerChoice === "S") ||
+//     (playerChoice === "P" && computerChoice === "R") ||
+//     (playerChoice === "S" && computerChoice === "P")
+//   ) {
+//     result = "You win!";
+//     wins++;
+//   } else {
+//     result = "You lose!";
+//     losses++;
+//   }
+
+
+//   alert(`You chose ${playerChoice}. Computer chose ${computerChoice}. ${result}`);
+
+//   const playAgain = confirm("Do you want to play again?");
+//   if (playAgain) {
+//     play();
+//   } else {
+//     alert(`Total Wins: ${wins}\nTotal Ties: ${ties}\nTotal Losses: ${losses}`);
+//   }
+// }
+
+
+
+
+let options = ["Rock", "Paper", "Scissors"]
 let wins = 0;
-let losses = 0;
-let ties = 0;
+let looses = 0;
+let ties =0;
 
-let roundNumber = 0;
+let computerChoice 
+let PlayerChooses 
 
-let playerChoice = prompt("Enter 'R' for Rock, 'P' for Paper, or 'S' for Scissors")
+let computer
+let player
 
-let randomIndex = Math.floor(Math.random() * choices.length)
-let computerChoice = choices[randomIndex];
 
-let result = "";
+let computerWins = function(){
 
-play()
-
-function play() {
-
-  if (playerChoice === "R" && computerChoice === "P") {
-
-    console.log("player wins!")
-
-    wins++
+  if (Rock) {
+    console.log("Computer Wins")
+    
   }
-  else if (
-    (playerChoice === "R" && computerChoice === "S") ||
-    (playerChoice === "P" && computerChoice === "R") ||
-    (playerChoice === "S" && computerChoice === "P")
-  ) {
-    result = "You win!";
-    wins++;
-  } else {
-    result = "You lose!";
-    losses++;
-  }
-
-
-  alert(`You chose ${playerChoice}. Computer chose ${computerChoice}. ${result}`);
-
-  const playAgain = confirm("Do you want to play again?");
-  if (playAgain) {
-    play();
-  } else {
-    alert(`Total Wins: ${wins}\nTotal Ties: ${ties}\nTotal Losses: ${losses}`);
+  else{
+    
   }
 }
